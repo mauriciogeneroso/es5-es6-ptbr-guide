0d60d6d48cecbc292eea16a1754474ee713d4bc3
Maurício Generoso <mauro_generoso@hotmail.com>
2019-03-10 17:44:52 -0300

    update escopo

--- es6/2_escopo/exemplo_let.js
+++ es6/2_escopo/exemplo_let.js
@@ -2,5 +2,5 @@ for (let i = 0; i < 3; i++) {
     let algumaCoisa = 'Eu não tenho escopo de bloco';
 }
 
-console.log('Valor de i: ' + i);
-console.log(algumaCoisa);
+console.log('Valor de i: ' + i); // Error
+console.log(algumaCoisa); // Error
